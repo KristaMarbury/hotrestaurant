d2ff5bc83339c366abe2dd01339f84b176d594ab
Krista Marbury <laetaiustus@gmail.com>
2021-06-08 21:29:07 -0500

    finished the mini project

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 // dependencies
+const { table } = require("console");
 const express = require("express");
 const path = require('path');
 
 //tells node that we are creating an express server
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //set initial port
 const PORT = process.env.PORT || 3001;
 
 //database
 const tables = [
-    {
-      customerName: "K-Leigh",
-      phoneNumber: "18001800",
-      customerEmail: "nah@yahoo.com",
-      customerID: "255366",
-    },
+  {
+    customerName: "K-Leigh",
+    phoneNumber: "18001800",
+    customerEmail: "nah@yahoo.com",
+    customerID: "255366",
+  },
+  {
+    customerName: "Dick Grayson",
+    phoneNumber: "18001800",
+    customerEmail: "noice@yahoo.com",
+    customerID: "254364",
+  },
 ];
 
 const waitlist = [
@@ -30,6 +40,34 @@ const waitlist = [
 // Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, './public/home.html'));
+});
+
+app.get("/tables", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/tables.html"));
+});
+
+app.get("/reservation", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/reservation.html"));
+});
+
+app.post("/api/tables", (req, res) => {
+    const newTable = req.body;
+    if (tables.length < 5) {
+        tables.push(newTable);
+        res.json(true);
+    } else {
+        waitlist.push(newTable);
+        res.json(false);
+    }
+    
+});
+
+//clear out the data
+app.post('/api/clear', (req, res) => {
+    tables.length = 0;
+    waitlist.length =0;
+
+    res.json({ ok: true });
 });
 
 app.get("/api/tables", (req, res) => res.json(tables));
